ec574602ace4716844427d5937a9ea9edb2593f4
s203 <s203@ai241lab0203m05.aipdcls.edu>
2017-07-27 16:54:10 -0700

    cents

---
+++ change-return.py
@@ -0,0 +1,16 @@
+print("Hi there, need change?")
+
+#setup
+total_change = 100
+
+#input
+print('How much do you want back?')
+change = float(input())
+
+#transform
+quarter = total_change // .25
+
+#output
+print(str(change) + ' cahnge is ' + str(quarter) + ' quarter. ')
+
+#couldn't find how to set cent right anf not just math
