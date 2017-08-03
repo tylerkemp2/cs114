0eb3059b6cb5d8d901299fb1ea04098d3f572dac
s203 <s203@ai241lab0203m05.aipdcls.edu>
2017-08-03 16:57:30 -0700

    madlib

---
+++ practice:madlib.py
@@ -0,0 +1,59 @@
+from time import sleep
+
+
+
+print('Let us do a madlib')
+sleep(2)
+
+#setup
+
+
+#input
+print('We need Adjection, describes something like cute or squsihy :)')
+adj1 = input()
+print('We need Exclamation like WOW! or OH!. Make sure it has this !')
+exl1 = input()
+print('Another adjection sir')
+ADJ2 = input()
+print('We need Verb, describes an action like run or jump.')
+V1 = input()
+print('We need Number, like 1 or 69.....ha')
+N1 = input()
+print('We need Color, like red, but you should pick something color:)')
+C1 = input()
+print('We need Plural noun, describes person, place, or thing like you :)')
+PN1 = input()
+sleep(1)
+print('Unless your a computer virse or computer zombie.')
+sleep(1)
+print('Then please dont kill or eat my brain')
+sleep(2)
+print('Okay now a Exlcamation.')
+EXL2 = input()
+print('We need adverb, describes an action like quickly or silently')
+ADV1 = input()
+print('Cool More adjection sir.')
+ADJ3 = input()
+print('A noun now, something like plural noun.')
+NO2 = input()
+print('we need adjection')
+ADJ4 = input()
+print('Another adjection, Wow we have alot dont we')
+ADJ5 = input()
+sleep(2)
+print('Give me something I need to write this in.')
+sleep(3)
+print('sorry for the wait almost done.')
+sleep(2)
+#transform
+
+
+#output
+print('I am done, here look.')
+sleep(1)
+print('There was once a '+ str(adj1) +' cow that said '+ str(exl1) +' whenever she was milked.')
+print('Her milk was very '+ str(ADJ2) +', and whoever drunk it just wanted to '+ str(V1)+'.')
+print('.The man who owned the cow bought her for '+ str(N1) +''+ str(C1) +''+ str(PN1))
+print('He thought to himself '+ str(EXL2) +' I never thought I would buy a cow like this.')
+print('He '+ str(ADV1) +' rushed the cow home and put it under his '+ str(ADJ3) +''+ str(NO1))
+print('Then called all of his '+ str(ADJ4) +' friends and invited them over to have a '+ str(ADJ5) +' cow party.')
--- wallprint.py
+++ wallprint.py
@@ -17,6 +17,7 @@ cost = float(input())
 square_feet = width * lenght
 gallons = square_feet / 400
 total_cost = gallons * cost
+total_square = wall * square_feet
 
 #output
 print('5 seconds are need for me to calculate')
@@ -27,3 +28,6 @@ print('That is a cool color')
 sleep(5)
 print('The total square feet is '+ str(square_feet))
 print('The total cost is $'+ str(total_cost))
+print('How many walls is this for?')
+wall = float(input())
+print('Then your total square feet is '+ str(total_square))
